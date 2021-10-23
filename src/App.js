0cf69bf74561ddd1e4a11c2b9c6041ccdd447970
Jardy17 <78389668+Jardy17@users.noreply.github.com>
2021-10-23 14:15:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles-1.css';
+import Interfas1 from './interfas1';
+import Interfas2 from './interfas2';
+import Interfas3 from './interfas-3';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-padre">
+    <Interfas1 />
+   
+    <Interfas2 name=" Jardy Litardo" age=" 17" addres=" Vinces-Ecuador" remote=" Available" ocupation=" FrontEnd" evento={function(){
+     alert("El boton funciona")}}/>
+
+<p className="primer-name-menu">ABILITIES</p>
+
+  <div className="container-3">
+    
+     <Interfas3 pagina="none" 
+     alt="HTML" 
+     name="HTML" 
+     parrafo="Knowledge modeling with the HTML markup language."/>
+
+     <Interfas3 pagina="none" 
+     alt="CSS" 
+     name="CSS" 
+     parrafo="Intermediate-advanced knowledge implementing styles with CSS."/>
+
+     <Interfas3 pagina="none" 
+     alt="Js" 
+     name="JS" 
+     parrafo="Intermediate knowledge implementing functionalities to websites with JavaScritp."/>
+     
+     <Interfas3 pagina="none" 
+     alt="React.js" 
+     name="REACT.JS" 
+     parrafo="Intermediate-advanced knowledge developing interfaces and web functionalities with React.js."/>
+  </div>
+  
     </div>
   );
 }
 
 export default App;
+
